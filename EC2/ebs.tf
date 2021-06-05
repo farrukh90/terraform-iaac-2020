8657f73381afb3227cae937bc997d20f6947b7d6
farrukh90 <farrukhsadykov@gmail.com>
2021-06-05 14:03:44 +0000

    added ec2

--- EC2/ebs.tf
+++ EC2/ebs.tf
@@ -6,3 +6,10 @@ resource "aws_ebs_volume" "example" {
     Name = "HelloWorld"
   }
 }
+
+
+resource "aws_volume_attachment" "ebs_att" {
+  device_name = "/dev/sdh"
+  volume_id   = aws_ebs_volume.example.id
+  instance_id = aws_instance.web.id
+}
